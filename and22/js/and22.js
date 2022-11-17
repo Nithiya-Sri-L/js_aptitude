17f9de781c08e4329bee5c0cdfa2411ea0929ce5
nithiya <nithiyasrisri3@gmail.com>
2022-11-17 16:39:39 +0530

    adding 1 js

---
+++ and22/js/and22.js
@@ -0,0 +1,15 @@
+var a =parseInt(prompt("enter the number"));
+var b =parseInt(prompt("enter the number"));
+var c =parseInt(prompt("enter the number"));
+if (a>=b)&&(a>=c)
+{
+	alert("a is greater");
+}
+else if (b>=a && b>=c)
+{
+	alert(" b is greater")
+}
+else
+{
+	alert("c is greater")
+}
