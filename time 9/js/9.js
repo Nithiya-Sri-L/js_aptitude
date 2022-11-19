bc22469d8001d4e132107acd75d2d1e9eda02552
nithiya <nithiyasrisri3@gmail.com>
2022-11-19 16:00:35 +0530

    adding file

---
+++ time 9/js/9.js
@@ -0,0 +1,8 @@
+var dis=prompt("enter the distance");
+var speed=prompt("enter the speed");
+var time;
+dis=parseInt(dis);
+speed=parseInt(speed);
+var time=dis/speed;
+time=time*(18/5);
+alert(time);
