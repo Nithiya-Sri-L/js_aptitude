ef7269a7a45db2f91342264ddf4ac5d232d363ac
nithiya <nithiyasrisri3@gmail.com>
2022-11-19 15:47:59 +0530

    add file

---
+++ profit 6/js/6.js
@@ -0,0 +1,8 @@
+var cp=parseInt(prompt("enter the cp"));
+var sp=parseInt(prompt("enter the sp"));
+var profit;
+profit=cp-sp;
+alert(profit);
+var p;
+p=(profit/cp)*100;
+alert(p);
---
+++ profit2/js/2.js
@@ -0,0 +1,8 @@
+var sp=parseInt(prompt("enter the sp"));
+var cp=parseInt(prompt("enter the cp"));
+var profit;
+profit=sp-cp;
+alert(profit);
+var p;
+p=(profit/cp)*100;
+alert(p);
