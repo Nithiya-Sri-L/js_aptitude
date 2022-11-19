fa74383254e45425e32cbca04c16bbc3460bb850
nithiya <nithiyasrisri3@gmail.com>
2022-11-19 16:01:32 +0530

    adding file

---
+++ length 10/js/bridge10.js
@@ -0,0 +1,9 @@
+var speed=prompt("enter the speed");
+var time=prompt("enter the time");
+var time=time*60;
+var dis;
+speed=parseInt(speed);
+time=parseInt(time);
+var dis=speed*time;
+dis=dis*(5/18);
+alert(dis);
