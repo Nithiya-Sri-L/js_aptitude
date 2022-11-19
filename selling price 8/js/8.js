a0fedfec242902daadcb4712268c968b94a1157c
nithiya <nithiyasrisri3@gmail.com>
2022-11-19 15:59:42 +0530

    adding file

---
+++ selling price 8/js/8.js
@@ -0,0 +1,15 @@
+var sp=parseInt(prompt("enter the sp"));
+var loss=parseInt(prompt("enter the loss"));
+var cp;
+cp=((100/(100-loss))*sp);
+alert(cp);
+var cp=parseInt(prompt("enter the cp"));
+var profit=parseInt(prompt("enter the profit"));
+var sp;
+sp=(((100+profit)/100)*cp);
+alert(sp);
+
+
+
+
+
