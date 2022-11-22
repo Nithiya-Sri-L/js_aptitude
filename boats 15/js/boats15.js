79683539472bc5f8b33ddd2b17751c39fe51309a
nithiya <nithiyasrisri3@gmail.com>
2022-11-22 15:03:23 +0530

    base file

---
+++ boats 15/js/boats15.js
@@ -0,0 +1,6 @@
+var stillSpeed=prompt("enter the stillSpeed");
+var upSpeed=prompt("enter the upSpeed");
+stillSpeed=parseInt(stillSpeed);
+upSpeed=parseInt(upSpeed);
+var streamSpeed=stillSpeed-upSpeed;
+alert(streamSpeed);
