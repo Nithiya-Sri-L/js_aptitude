95a30985450bd7f513add3baa0db2c21765e9150
nithiya <nithiyasrisri3@gmail.com>
2022-11-22 15:17:34 +0530

    base file

---
+++ function 39/js/function.js
@@ -0,0 +1,11 @@
+function nithi(a,b,c)
+{
+	return [a,b,c];
+}
+d=parseInt(prompt("enter the value"));
+e=parseInt(prompt("enter the value"));
+f=parseInt(prompt("enter the value"));
+x=nithi(d,e,f);
+alert(x);
+
+
